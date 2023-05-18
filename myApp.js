7417830ceb192302f8bf4d11f1d6537e7f1ed2fe
tomascappelli <tomascappelli@hotmail.com>
2023-05-18 10:16:47 -0300

    DNS Prefetch OFF

--- myApp.js
+++ myApp.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
+
+ninetyDaysInSeconds = 90*24*60*60;
+
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
-
+app.use(helmet.dnsPrefetchControl());
 
 
 
