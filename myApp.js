cae3ea184abe88e7288c5a30b8de66ab09fb1357
tomascappelli <tomascappelli@hotmail.com>
2023-05-18 10:18:25 -0300

    No Cache ON

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
-
+app.use(helmet.noCache());
 
 
 
