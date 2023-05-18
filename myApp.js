a85c733f81d5b79aa8d74f864a370fd191af7f9f
tomascappelli <tomascappelli@hotmail.com>
2023-05-18 10:30:27 -0300

    Ordenando un poco

--- myApp.js
+++ myApp.js
@@ -4,65 +4,17 @@ const helmet = require('helmet');
 
 ninetyDaysInSeconds = 90*24*60*60;
 
-app.use(helmet.hidePoweredBy());
-
-app.use(helmet.frameguard({action: 'deny'}));
-
-app.use(helmet.xssFilter());
-
-app.use(helmet.noSniff());
-
-app.use(helmet.ieNoOpen());
-
-app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
-
-app.use(helmet.dnsPrefetchControl());
-
-app.use(helmet.noCache());
-
-app.use(helmet.contentSecurityPolicy({
+app.use(helmet({
+  frameguard: {
+    action: 'deny' 
+  },
+  contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
     scriptSrc: ["'self'",'trusted-cdn.com'],
-  }
+  },
+  dnsPrefetchControl:false
 }));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
 const api = require('./server.js');
