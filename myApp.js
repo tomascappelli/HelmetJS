7de19c4dff85f66a156a19a9758d33113269336f
tomascappelli <tomascappelli@hotmail.com>
2023-05-18 10:14:18 -0300

    Forcing HTTPS

--- myApp.js
+++ myApp.js
@@ -6,7 +6,8 @@ app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-
+ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 
 
