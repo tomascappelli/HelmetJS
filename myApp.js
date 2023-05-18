964d825ea93d795c8a93cd1d00db03b49676a03e
tomascappelli <tomascappelli@hotmail.com>
2023-05-18 10:25:45 -0300

    Content Policy

--- myApp.js
+++ myApp.js
@@ -5,15 +5,27 @@ const helmet = require('helmet');
 ninetyDaysInSeconds = 90*24*60*60;
 
 app.use(helmet.hidePoweredBy());
+
 app.use(helmet.frameguard({action: 'deny'}));
+
 app.use(helmet.xssFilter());
+
 app.use(helmet.noSniff());
+
 app.use(helmet.ieNoOpen());
+
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+
 app.use(helmet.dnsPrefetchControl());
+
 app.use(helmet.noCache());
 
-
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'","'trusted-cdn.com'"],
+  }
+}));
 
 
 
